05b492aea7aac7ea119864c8d59c6b9f60264553
Castillo316 <115067244+Castillo316@users.noreply.github.com>
2023-11-10 16:14:55 -0600

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+/* ============================ typing animation ==================================*/
+var typed = new Typed(".typing", {
+    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    Backspeed:80,
+    loop:true
+})
+
+
+
+
+
+
